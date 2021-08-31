4d3031e30b7d1006ffcc22bb9e5a89d1e583465d
gfike19 <gfike19@gmail.com>
2021-08-31 16:19:18 -0500

    started work on use of wildcard in regex

--- ch25.py
+++ ch25.py
@@ -1,12 +1,32 @@
 import re
 
-# match the before group 0 or one times = ?
-batRegex = re.compile(r'Bat(wo)?man')
+# match 1 or one times = ?
+# batRegex = re.compile(r'Bat(wo)?man')
+# mo = batRegex.search('The Adventures of Batman')
+# print(mo.group())
+# mo = batRegex.search('The Adventures of Batwoman')
+# print(mo.group())
+
+phoneRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phoneRegex.search('My phone number is 415-555-1234. Call me'+
+'tomorrow')
+# above regex will work
+# print(mo.group())
+mo = phoneRegex.search('My phone number is 555-1234. Call me'+
+'tomorrow')
+# this one won't
+# print(mo == None)
+
+# to resolve, group first chunk and place ? beside it
+phoneRegex = re.compile(r'(\d\d\d-)?\d\d\d-\d\d\d\d')
+# print(phoneRegex.search('My phone number is 555-1234. Call me'+
+# 'tomorrow'))
+
+# * star aka wildcard matach 0 or more times
+batRegex = re.compile(r'Bat(wo)*man')
 mo = batRegex.search('The Adventures of Batman')
 print(mo.group())
 mo = batRegex.search('The Adventures of Batwoman')
 print(mo.group())
-
-phoneRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
-phoneRegex.search('My phone number is 415-555-1234. Call me'+
-'tomorrow')
+mo = batRegex.search('The Adventures of Batwowowoman')
+print(mo.group())
