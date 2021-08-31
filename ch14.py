406fe4d005adffb2ac16d1bf5b7cbf9724ce91cd
gfike19 <gfike19@gmail.com>
2021-08-31 09:54:33 -0500

    adding files

---
+++ ch14.py
@@ -0,0 +1,27 @@
+# [] is a sequence not a list
+# for i in [0,1,2,3]:
+#     print(i)
+
+# assign values from range to list/array
+# lst = list(range(0, 4))
+# print(lst)
+
+# above useful for mapping large amount of numbers
+# print(list(range(0,101, 2)))
+
+# multiple assignment operator
+# cat = ['fat', 'orange', 'loud']
+# size, color, disposition = cat
+# print(size)
+# print(color)
+# print(disposition)
+
+# size, color, disposition = 'skinny', 'black', 'quiet'
+# print(size)
+# print(color)
+# print(disposition)
+
+a = 'AAA'
+b = 'BBB'
+a, b = b, a
+# values will be reversed
---
+++ ch24.py
@@ -0,0 +1,4 @@
+import re
+
+message = "call me at 314-967-5309, or at 314-555-5555"
+phoneNumberRegex = re.compile('\d\d\d-\d\d\d-\d\d\d\d')
