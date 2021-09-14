d5225c571f1c0ac4457a61a62e3c71fa88fda99e
gfike19 <gfike19@gmail.com>
2021-09-14 09:20:39 -0500

    stopped at discussion of pipe usage with verbose method

--- lesson28.py
+++ lesson28.py
@@ -14,3 +14,11 @@ print(namesRegex.findall("Agent Alice gave the secret documents to Agent Bob"))
 print(namesRegex.sub(r'Agent \1****', "Agent Alice gave the secret documents to Agent Bob"))
 # below will replace Agent and their name but space still remains
 print(namesRegex.sub(r'\0REDACTED', "Agent Alice gave the secret documents to Agent Bob"))
+# re.verbost removes whitespace as part of the string
+# so below the new line character won't be present
+re.compile('''
+\d\d\d
+-
+\d\d\d
+-
+\d\d\d\d''', re.VERBOSE)
