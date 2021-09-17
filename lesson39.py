96e7b9a5c6c19083277a3706e0c2f51c12b55193
gfike19 <gfike19@gmail.com>
2021-09-17 16:15:47 -0500

    finished lesson 39

--- lesson39.py
+++ lesson39.py
@@ -1,3 +1,13 @@
 import requests
 
-requests.get("https://miro.medium.com/max/1050/1*1BUIofZgqVuR6nj8LbrRtQ.jpeg")
+res = requests.get("https://miro.medium.com/max/1050/1*1BUIofZgqVuR6nj8LbrRtQ.jpeg")
+# print(res.status_code)
+# print(res.text[:500])
+# easy way to see if it's working
+print(res.raise_for_status())
+# returns none if all good
+# can save web page but must do it with binary file (so wb instead of w)
+testFile = open("testFile.jpg", "wb")
+for chunk in res.iter_content(1000000):
+    testFile.write(chunk)
+testFile.close()
